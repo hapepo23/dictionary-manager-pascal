5c99dbb832e8a6c21851afd82eac88aa046f1280
hapepo23 <github@hapepo.de>
2025-10-23 09:42:49 +0200

    update 2025-10-23 09:42 CEST

--- udictentry.pas
+++ udictentry.pas
@@ -15,11 +15,8 @@ type
     sKey: string;
     sData: string; // NL-delimited
   public
-    constructor Create(pLine: string); // key TAB data1 TAB data2 ...
-    function IsKeyValid(keys: TStringList): boolean;
-    procedure GetDataInto(sl: TStringList);
-    function ToString: string; override; // key TAB data1 TAB data2 ...
-    function Display: string; // for Debugging
+    constructor Create(pLine: string); // TAB-delimited
+    function ToString: string; override; // TAB-delimited
     property Key: string read sKey write sKey;
     property Data: string read sData write sData;
   end;
@@ -44,41 +41,12 @@ begin
   end;
 end;
 
-function TDictEntry.IsKeyValid(keys: TStringList): boolean;
-begin
-  if sKey = '' then
-    Result := False
-  else if keys.IndexOf(sKey) <> -1 then
-    Result := False
-  else
-    Result := True;
-end;
-
-procedure TDictEntry.GetDataInto(sl: TStringList);
-var
-  Parts: TStringDynArray;
-  i: integer;
-begin
-  Parts := sData.Split([LineEnding]);
-  sl.Clear;
-  for i := Low(Parts) to High(Parts) do
-    sl.Add(Parts[i]);
-end;
-
 function TDictEntry.ToString: string;
 var
   Parts: TStringDynArray;
 begin
   Parts := sData.Split([LineEnding]);
   Result := sKey + #9 + string.Join(#9, Parts);
-end;
-
-function TDictEntry.Display: string;
-var
-  Parts: TStringDynArray;
-begin
-  Parts := sData.Split([LineEnding]);
-  Result := 'key="' + sKey + '", data="' + string.Join('|', Parts) + '"';
 end;
 
 end.
